c72a7e20585285424b8299efbe9dcc7a286eeab4
jakub gniewkowski <199080887+Obrzezanymichal@users.noreply.github.com>
2026-03-03 10:40:41 +0100

    Create skrypt1.php

---
+++ skrypt1.php
@@ -0,0 +1,78 @@
+<?php
+// // 1 zadanie
+// $liczba = 5;
+// if ($liczba % 2 == 0) {
+//     echo "Parzysta";
+// } else {
+//   echo "Nieparzysta";
+// }
+
+// //2 zadanie
+
+// $liczba = readline("podaj pierwsza liczbe: ");
+// $liczba2 = readline("podaj druga liczbe: ");
+
+// if ($liczba2 == 0) {
+//     echo "nie mozna dzielic przez 0";
+// } else if ($liczba % $liczba2 == 0) {
+//     echo "pierwsza liczba jest podzielna przez druga";
+// } else {
+//     echo "pierwsza liczba nie jest podzielna przez druga";
+// }
+
+// //zadanie 3
+
+// $liczba = readline("Podaj liczbe: ");
+
+// if (($liczba >= 1 && $liczba <= 10) || ($liczba >= 17 && $liczba <= 21)) {
+//     echo "liczba nalezy do jednego z przedzialow";
+// } else {
+//     echo "liczba nie nalezy do zadnego z przedzialow";
+// }
+
+// //zadanie 4
+
+// // $liczba = readline("podaj liczbe");
+
+// if ($liczba > 0) {
+//     echo "liczba jest wieksza od zera";
+// } else if ($liczba < 0) {
+//     echo "liczba jest mniejsza od zera";
+// } else {
+//     echo "liczba jest rowna zero";
+// }53
+
+// // zadanie 5
+
+// $liczba = readline("podaj wiek: ");
+
+// if ($liczba <11) {
+//     echo "kategoria dziecko";
+// } else if ($liczba > 11 && $liczba<17){
+//     echo "kategoria nastolatek";
+// } else if ($liczba > 17) {
+//     echo "kategoria dorosly";
+// }
+// zadanie 6
+
+// $haslo = readline("podaj haslo");
+// $liczbaznakow = strlen($haslo);
+
+// if ($liczbaznakow < 5) {
+//     echo "bardzo slabe haslo"
+// } else if ($liczbaznakow >=5 && $liczbaznakow <=8 ) {
+//     echo "slabe haslo"
+// }  else if ($liczbaznakow >=9 && $liczbaznakow <=11 ) {
+//     echo "silne haslo"
+// }  else if ($liczbaznakow > 11 ) {
+//     echo "bardzo silne haslo";
+// }
+
+
+
+
+
+
+
+
+?>
